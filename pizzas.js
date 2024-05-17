08e7bdb6b572d75fc781971c198f7d43e3435d27
SzitaiB <szitaib@kkszki.hu>
2024-05-17 08:53:42 +0200

    feltöltve

--- pizzas.js
+++ pizzas.js
@@ -162,4 +162,20 @@ searchInput.addEventListener('input', () => {
     }
 });
 
-
+JS
+fetch("https://pizza.kando-dev.eu/Pizza")
+.then((res) => res.json())
+.then((datas) => {
+    console.log(datas);
+    let content = document.getElementById("pizza-menu");
+    for(const data of datas) {
+        content.innerHTML += `<div class="card" style="width: 18rem;">
+        <img src="${data.kepURL}" class="card-img-top" alt="...">
+        <div class="card-body">
+          <h5 class="card-title">${data.name}</h5>
+          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+          <a href="#" class="btn btn-primary">Go somewhere</a>
+        </div>
+      </div>`
+    }
+})
